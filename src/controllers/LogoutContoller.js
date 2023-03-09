0c98cd47839f184a0bcb28747ddac682fb87d873
Vini-P <viniciuspedrosoreis@gmail.com>
2023-03-09 15:08:00 -0300

    [Lnkr-4] Logout done

--- src/controllers/LogoutContoller.js
+++ src/controllers/LogoutContoller.js
@@ -1,6 +1,19 @@
+import connectionDB from "../database";
 
 
-export default function Logout (req,res){
+export default async function Logout (req,res){
 
-    
+    const userData = res.locals.userData;
+
+    try {
+
+        await connectionDB.query("DELETE FROM sessions WHERE user_id=$1;",
+        [userData.id]);
+
+        return res.sendStatus(200)
+
+    }catch(err){
+        return res.status(500).send(err.message);
+    }
+
 }
